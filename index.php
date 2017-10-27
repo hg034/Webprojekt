59c849bf57ff5264fa06e3c2f4723d96ba4aa579
hannagrill <hanna.grill@web.de>
2017-10-27 18:27:14 +0200

    Test

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hanna
- * Date: 25.10.17
- * Time: 13:35
- */
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Webprojekt 2017</title>
+</head>
+
+<body>
+<h1>Möbel Online</h1>
+
+<p>Dies ist eure erste PHP-Datei. Eine Scriptumgebung könnt ihr wie folgt starten:
+    <?php
+    echo "Mittels echo können Daten ausgegeben werden";
+    ?></p>
+
+<p>Später könnt ihr in PHP dynamische Inhalte erzeugen. Ein einfaches Beispiel ist das aktuelle Datum auszugeben:
+    <?php
+    echo date("d.m.Y H:i:s");
+    ?></p>
+
+</body>
+</html><?php
+
